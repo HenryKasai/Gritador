ca74a62ae5a4edc7219b7faaad00c255ff729762
HenryKasai <henrykasai1775@gmail.com>
2020-05-11 11:35:12 -0300

    Exercício resolvido.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Digite algo.");
+            string grito = Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(grito.ToUpper());
+            Console.WriteLine("Okay... Por que você está gritando comigo?");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
